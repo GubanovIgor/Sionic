fd709e80bb0351d0edc91739b621dadf7f8a0a7f
Игорь <applecake@inbox.ru>
2020-06-30 15:56:40 +0300

    added icons to navigation

--- front/src/navigation/BottomNavigation.js
+++ front/src/navigation/BottomNavigation.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
 import {MainNavigation} from './MainNavigation';
 import {OrderNavigation} from './OrderNavigation';
 import {CartNavigation} from './CartNavigation';
-import { THEME } from '../theme';
+import {THEME} from '../theme';
+import {AppIcon} from '../components/AppIcon';
+import {Platform} from 'react-native';
 
 const BottomNavigator = createBottomTabNavigator();
 
 export const BottomNavigation = () => {
   return (
     <BottomNavigator.Navigator tabBarOptions={tabBarOptions}>
-      <BottomNavigator.Screen name="Main" component={MainNavigation} />
-      <BottomNavigator.Screen name="Cart" component={CartNavigation} />
-      <BottomNavigator.Screen name="Orders" component={OrderNavigation} />
+      <BottomNavigator.Screen
+        name="Main"
+        component={MainNavigation}
+        options={bottomMainScreenOptions}
+      />
+      <BottomNavigator.Screen
+        name="Cart"
+        component={CartNavigation}
+        options={bottomCartScreenOptions}
+      />
+      <BottomNavigator.Screen
+        name="Orders"
+        component={OrderNavigation}
+        options={bottomOrdersScreenOptions}
+      />
     </BottomNavigator.Navigator>
   );
 };
 
 const tabBarOptions = {
-  showLabel: true,
+  showLabel: false,
   style: {
     ...(Platform.OS === 'android'
       ? {
@@ -34,4 +47,28 @@ const tabBarOptions = {
           shadowOpacity: 0,
         }),
   },
+};
+
+const bottomMainScreenOptions = () => {
+  return {
+    tabBarIcon: ({focused}) => (
+      <AppIcon focused={focused} name="home" size={24} />
+    ),
+  };
+};
+
+const bottomCartScreenOptions = () => {
+  return {
+    tabBarIcon: ({focused}) => (
+      <AppIcon focused={focused} name="shopping-cart" size={24} />
+    ),
+  };
+};
+
+const bottomOrdersScreenOptions = () => {
+  return {
+    tabBarIcon: ({focused}) => (
+      <AppIcon focused={focused} name="list-alt" size={24} />
+    ),
+  };
 };
--- front/src/theme.js
+++ front/src/theme.js
@@ -1,4 +1,5 @@
 export const THEME = {
   MAIN: '#6c42f5',
   DANGER: '#ff6a59',
+  LIGHT: '#ab91ff'
 }
